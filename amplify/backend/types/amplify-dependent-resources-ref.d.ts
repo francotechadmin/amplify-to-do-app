3de7ec70d71ba1d96a98a226e8a2e93b650b611d
Gabriel Franco <gabriel.a.franco@invista.com>
2024-12-14 15:10:57 -0600

    initialize project structure with Next.js, Tailwind CSS, and Amplify setup

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "tododemoa7cf11d8": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
