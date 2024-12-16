bd6c4b4088c35ccb4b0cb7966cb4e7030d72f0e0
Gabriel Franco <gabriel.a.franco@invista.com>
2024-12-16 11:20:40 -0600

    Add tododemo API with GraphQL schema, queries, mutations, and subscriptions

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -4,6 +4,11 @@ export type AmplifyDependentResourcesAttributes = {
       "ApiId": "string",
       "ApiName": "string",
       "RootUrl": "string"
+    },
+    "tododemo": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
     }
   },
   "auth": {
